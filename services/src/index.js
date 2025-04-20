618d2b551053b2f791b94911559c2d901ab0b615
Soumya Ranjan Barik <soumya.vedalytics@gmail.com>
2025-04-20 13:24:34 +0530

    fixing issues in backend

--- services/src/index.js
+++ services/src/index.js
@@ -22,7 +22,7 @@ app.use(helmet());
 app.use(morgan("common"));
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://netverse-social.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true, // add this if using cookies/auth headers
 };
