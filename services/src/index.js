0fa8ff2ae4a0e8dd6276ad436f6aae56e2998e92
Soumya Ranjan Barik <soumya.vedalytics@gmail.com>
2025-04-23 11:14:14 +0530

    fixing cors issue

--- services/src/index.js
+++ services/src/index.js
@@ -22,7 +22,7 @@ app.use(helmet());
 app.use(morgan("common"));
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://netverse-social.vercel.app"],
+  origin: "https://netverse-social.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 };
